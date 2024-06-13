409e2df7f08dd96b3affa822a97d57acfdde20dd
VrushikaRana <165312497+VrushikaRana@users.noreply.github.com>
2024-06-13 23:11:19 +0530

    Delete connection.php

--- connection.php
+++
@@ -1,5 +0,0 @@
-<?php
-$db=mysqli_connect ('localhost','root','')or
-    die('unable to connect.check your connection paramrteres.');
-    mysqli_select_db($db,'password_manager') or die(mysqli_error($db));
-?>
